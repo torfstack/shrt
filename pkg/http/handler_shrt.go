d401cb8bdd13af16d718a3083ca00d6e428e7d6e
David Deininger <torfstack@gmail.com>
2024-11-02 15:00:15 +0100

    feat: support not specifying schema

--- pkg/http/handler_shrt.go
+++ pkg/http/handler_shrt.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"encoding/base64"
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"net/http"
 	"shrt/pkg/service"
@@ -12,22 +14,37 @@ func Shrt(s service.ShrtService) func(c echo.Context) error {
 		switch p := c.Param("something"); {
 		case p == "":
 			return c.String(http.StatusBadRequest, "url is required")
-		case isUrl(p):
-			shortUrl, err := s.Shorten(p)
-			if err != nil {
-				return c.String(http.StatusInternalServerError, err.Error())
-			}
-			return c.String(http.StatusOK, shortUrl)
-		default:
+		case isBase64UrlEncoded(p):
 			url, err := s.Unshorten(p)
 			if err != nil {
 				return c.String(http.StatusInternalServerError, err.Error())
 			}
 			return c.Redirect(http.StatusMovedPermanently, url)
+		default:
+			shortUrl, err := s.Shorten(sanitizeUrl(p))
+			if err != nil {
+				return c.String(http.StatusInternalServerError, err.Error())
+			}
+			return c.String(http.StatusOK, shortUrl)
 		}
 	}
 }
 
-func isUrl(s string) bool {
-	return strings.HasPrefix(s, "http://") || strings.HasPrefix(s, "https://")
+func isBase64UrlEncoded(s string) bool {
+	_, err := base64.RawURLEncoding.DecodeString(s)
+	return err == nil
+}
+
+func sanitizeUrl(s string) string {
+	if strings.HasPrefix(s, "http://") ||
+		strings.HasPrefix(s, "https://") {
+		return s
+	}
+
+	_, err := http.DefaultClient.Get(fmt.Sprintf("https://%s", s))
+	if err == nil {
+		return fmt.Sprintf("https://%s", s)
+	}
+
+	return fmt.Sprintf("http://%s", s)
 }
